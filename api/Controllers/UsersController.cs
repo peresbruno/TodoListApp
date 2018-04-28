d450f9b6cf71468e62f5081a2732bb252f8995bb
Bruno Peres <bruno@localhost.localdomain>
2018-04-28 14:40:10 -0300

    first commit

---
+++ api/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        // GET
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {"User1", "User2"};
+        }
+    }
+}
